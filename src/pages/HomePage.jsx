2baa907c050123d6aed515849c675fbd4100d15f
taseenjeem <taseenjeem.dev@gmail.com>
2024-04-18 22:44:29 +0600

    jvk

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
-import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { getAuth, sendPasswordResetEmail, signOut } from "firebase/auth";
 import { useState } from "react";
 import app from "../auth/firebase-init";
 import Loading from "../components/Loading";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
   const [isLoading, setIsLoading] = useState(false);
   const auth = getAuth(app);
+  const redirect = useNavigate();
 
   const handleResetPass = async (event) => {
     setIsLoading(true);
@@ -25,12 +27,21 @@ const HomePage = () => {
     }
   };
 
+  const handleLogout = async () => {
+    await signOut(auth);
+    alert("Log out success");
+    redirect("/");
+  };
+
   return (
     <>
       {isLoading ? (
         <Loading />
       ) : (
         <div className="min-h-screen text-4xl font-bold flex flex-col justify-center items-center">
+          <button onClick={handleLogout} className="btn btn-primary">
+            Log Out
+          </button>
           <h1>This is your Home page</h1>
           <form onSubmit={handleResetPass} className="mt-9">
             <input
